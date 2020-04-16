5795696734676f1ec611adf93dc95b27d92d4d21
sunvirgo <g_sun2java@163.com>
2020-04-16 22:05:08 +0800

    修改文件

--- src/main/java/com/example/springbootday01/controller/HelloController.java
+++ src/main/java/com/example/springbootday01/controller/HelloController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String hello() {
+        System.out.println("OK");
         return "Hello World!";
     }
 }
